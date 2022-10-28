12e46bba30ee90ff2a54862d7aa73552b50e1136
rafaelmjardim <rafaelmjardim@hotmail.com>
2022-10-28 16:59:40 -0300

    Primeira estilização do header

---
+++ components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import styles from './Header.module.scss';
+import Image from 'next/image';
+import Logo from "../../public/logo.png";
+
+const Header = () => {
+    return (
+        <div className={styles.header}>
+            <div className={styles.header_logo}>
+                <Image 
+                    src={Logo}
+                    width={102}
+                    height={110.78}
+                />
+            </div>
+
+            <ul className={styles.header_menu}>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Habilidades</a></li>
+                <li><a href="#">Projetos</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+            
+            <div className={styles.header_action}>
+                <button className={styles.header_action_button}>Contatar</button>
+            </div>
+        </div>
+
+    );
+}
+
+export default Header;
