127be79a223f7f515f778e0b6d237cd67aaa9f55
rafaelmjardim <rafaelmjardim@hotmail.com>
2022-11-07 13:51:06 -0300

    Update card end script google

--- components/StartProject/StartProject.tsx
+++ components/StartProject/StartProject.tsx
@@ -1,5 +1,5 @@
 import styles from './StartProject.module.scss';
-
+import Link from 'next/link';
 import Image from 'next/image';
 import handup from '../../public/icons/handup.png';
 
@@ -12,10 +12,12 @@ const StartProject = () => {
              Devíamos arranjar uma hora para conversar.
              Vou preparar o café.</p>
 
-             <button className={styles.start_project_button}>
-                <Image src={handup}></Image> 
-                Vamos fazer isso
-            </button>
+            <Link href={"https://api.whatsapp.com/send?phone=5551998747124&text=Ol%C3%A1%2C%20Gostaria%20de%20mais%20informa%C3%A7%C3%B5es!"}>
+                <button className={styles.start_project_button}>
+                    <Image src={handup}></Image>
+                    Vamos fazer isso
+                </button>
+            </Link>
         </div>
     );
 }
--- pages/ScriptGA.tsx
+++ pages/ScriptGA.tsx
@@ -3,16 +3,15 @@ import Script from "next/script";
 const ScriptGA = () => {
     return (
         <>
-                 {/* Google tag (gtag.js) */}
-                <script async src="https://www.googletagmanager.com/gtag/js?id=G-7RFG93YESW"></script>
-                <script>
+                <Script async src="https://www.googletagmanager.com/gtag/js?id=G-7RFG93YESW"></Script>
+                <Script>
                 
                 {`window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
 
                 gtag('config', 'G-7RFG93YESW');`}
-                </script>
+                </Script>
         </>
     );
 }
